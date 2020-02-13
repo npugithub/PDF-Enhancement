1f5ce2272447e0532a629cbbee46c7eacac0d37e
529HenryMJi <jeep.jipu@gmail.com>
2020-02-13 16:56:56 +0800

    initial

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from mylib import convert
 import os
 
 
-
 def makedir(pdf_name):
     png_dir = pdf_name[:-4]
     if not os.path.exists(png_dir):
@@ -20,7 +19,7 @@ def makedir(pdf_name):
 if __name__ == '__main__':
     pdf_name = 'test.pdf'
     current_path = os.getcwd() + '/'
-    pdf_name_new = pdf_name[:-4]+'_converted'+pdf_name[-4:]
+    pdf_name_new = pdf_name[:-4] + '_converted' + pdf_name[-4:]
 
     # 获取原PDF的信息
     information = info.get_info(pdf_name)
@@ -30,22 +29,31 @@ if __name__ == '__main__':
     png_dir = makedir(pdf_name)
 
     # PDF 转换为 PNGs
-    convert.pdf2png(pdf_name, png_dir)
-
-
+    # convert.pdf2png(pdf_name, png_dir)
 
     # 图像增强
 
 
+'''
+    test_image = mping.imread('test/page_0.png')
+    print(test_image.shape)
+
+    plt.imshow(test_image)  # 显示图片
+    # plt.axis('off')
+    plt.show()
+
+    data_1 = test_image[:,:,0]  # 显示图片的第一个通道
+
+    plt.imshow(data_1)
+    plt.show()
+
+    fig3 = plt.figure()
+
+    img = plt.imshow(data_1)  # 显示灰度图
+    img.set_cmap('gray')  # 'hot' 是热量图
+    plt.show()
+
+'''
 
     # PNGs 转化为 PDF
-    convert.png2pdf(png_dir, pdf_name_new)
-
-
-
-
-
-
-
-
-
+    # convert.png2pdf(png_dir, pdf_name_new)
--- mylib/DIP.py
+++ mylib/DIP.py
@@ -2,11 +2,14 @@
 
 from PIL import Image
 from PIL import ImageEnhance
+import matplotlib.pyplot as plt  # 用于显示图片
+import matplotlib.image as mping  # 用于读取图片
+from PIL import ImageFilter  # 滤波器
+import numpy as np
+import os
+import math
 
-# 原始图像
-image = Image.open('lena.jpg')
-image.show()
-
+'''
 # 亮度增强
 enh_bri = ImageEnhance.Brightness(image)
 brightness = 1.5
@@ -30,3 +33,63 @@ enh_sha = ImageEnhance.Sharpness(image)
 sharpness = 3.0
 image_sharped = enh_sha.enhance(sharpness)
 image_sharped.show()
+'''
+
+
+def gamma_correction(c, gamma, array):
+    # 伽马变换 s = c * r^gamma
+    # gamma < 1，会拉伸图像灰度级低的地区
+
+    array = c * np.power(array, gamma)
+    return array
+
+def log_transform(c, N, array):
+    # 对数变换 s = c * log_N[1+(N-1)*r]
+    # N 越大，对低灰度值部分的扩展越强
+
+    array = c * np.log(1+(N-1)*array)/np.log(N)
+    return array
+
+
+
+
+if __name__ == '__main__':
+    im = Image.open('../test/page_9.png')  # 打开图片
+    # im.show()
+    # print(im.format,im.size,im.mode)
+
+    # im_new = Image.new('RGB',(256,256))   # 创建新图
+    rate = 1.2
+    im_array = np.array(im)  # 将 PIL Image 图片转换为 numpy 数组
+
+    # im_filtered = im.filter(ImageFilter.SMOOTH)  # filter参数：检测边缘
+    # im_filtered.show()
+    # im_filtered.save('../test/page_9_filtered.png')
+
+    # enh_sha = ImageEnhance.Sharpness(im)
+    # sharpness = 3.0
+    # image_sharped = enh_sha.enhance(sharpness)
+    # image_sharped.show()
+
+    # im_filtered = image_sharped.filter(ImageFilter.SMOOTH)  # filter参数：检测边缘
+    # im_filtered.show()
+    # im_filtered.save('../test/page_9_filtered.png')
+
+    width,height = im.size
+    width_big = math.floor(width*rate)
+    height_big = math.floor(height*rate)
+    im_new = im.resize((width_big,height_big),Image.ANTIALIAS)
+    im_new.save('../page_9_big.png')
+
+    # 读取三个信道的数据
+    data_1 = im_array[:, :, 0]/np.max(im_array)
+    data_2 = im_array[:, :, 1]/np.max(im_array)
+    data_2 = im_array[:, :, 1]/np.max(im_array)
+
+    print(np.max(data_1))
+
+    '''
+    # L = im.convert('L')
+    # L.show()
+    # L.save('../test/new_page_0.png')
+    '''
